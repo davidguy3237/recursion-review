acfbc4a2f51395721dd7edd27ec7f478834b09da
davidguy3237 <davidtran3237@gmail.com>
2023-03-27 16:28:43 -0700

    Complete getElementsByClassName and stringifyJSON

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -3,8 +3,47 @@
 //   return document.getElementsByClassName(className);
 // };
 
+/*
+[
+  '<div class="targetClassName"></div>',
+  '<div class="otherClassName targetClassName"></div>',
+  '<div><div class="targetClassName"></div></div>',
+  '<div><div class="targetClassName"><div class="targetClassName"></div></div></div>',
+  '<div><div></div><div><div class="targetClassName"></div></div></div>',
+  '<div><div class="targetClassName"></div><div class="targetClassName"></div></div>',
+  '<div><div class="somediv"><div class="innerdiv"><span class="targetClassName">yay</span></div></div></div>'
+]
+*/
+
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  var body = document.body;
+
+  // body -> return []
+  // child 1 -> [
+
+  var traverseTree = function(element) {
+    var elems = [];
+
+
+    if (element.classList) {
+      if (element.classList.contains(className)) {
+        elems.push(element);
+      }
+
+
+      var children = element.childNodes;}
+
+    if (children) {
+      for (var i = 0; i < children.length; i++) {
+        elems = elems.concat(traverseTree(children[i]));
+      }
+    }
+
+    return (elems);
+  };
+
+  return (traverseTree(body));
+
+
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,75 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  console.log(obj);
+  //var output = '';
+
+  if (typeof obj === 'function') {
+    return;
+  }
+
+  if (obj === undefined) {
+    return;
+  }
+
+  if (obj === null) {
+    return ('null');
+  }
+
+  if (typeof obj === 'boolean') {
+    if (obj === true) {
+      return ('true');
+    }
+    return ('false');
+  }
+
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+
+  if (typeof obj === 'number') {
+    return obj.toString();
+  }
+
+  if (Array.isArray(obj)) {
+
+    var output = '';
+
+    output += '[';
+
+
+    for (var i = 0; i < obj.length; i++) {
+      output += stringifyJSON(obj[i]);
+      output += ',';
+    }
+    output += ']';
+    output = output.replace(',]', ']');
+
+    return (output);
+  }
+
+  else if (typeof obj === 'object') {
+    var output = '';
+    output += '{';
+
+
+    for (const key in obj) {
+      if (obj[key] === undefined || typeof obj[key] === 'function') {
+        continue;
+      }
+
+      output += '"' + key + '"';
+      output += ':';
+      output += stringifyJSON(obj[key]);
+      output += ',';
+    }
+
+
+
+    output += '}';
+    output = output.replace(',}', '}');
+    return (output);
+  }
+
+  return output;
 };
